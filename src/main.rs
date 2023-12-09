ad0a36c6629f3f09e036547aeea78643fc5e2a70
Jingun Hong <jingun.hong@gmail.com>
2023-12-09 18:27:55 +0900

    add default plugins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.5,
+    })
 }
